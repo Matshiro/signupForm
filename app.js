a765c39d948f54bf390d4c476d81090ae567a1af
Matshiro <daniel.b.lewinski@gmail.com>
2023-02-23 23:03:54 +0100

    fixed bugs with password inputs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const formInput = document.querySelectorAll("input");
 const formLabel = document.querySelectorAll("label");
+const userPassword = document.getElementById("user_password");
+const userConfirmedPassword = document.getElementById("confirmPassword");
+const dontMatchMessage = document.getElementById("smallText");
 
 for(let i=0; i < formInput.length; i++){
-    formInput[i].addEventListener('blur', (e) =>{
+    formInput[i].addEventListener('focusout', (e) =>{
         if(formInput[i].value.length >= 1 && !formInput[i].checkValidity() ){
             formInput[i].style.borderColor = "#FF0000";
             formLabel[i].style.color = "#FF0000";
@@ -11,6 +14,27 @@ for(let i=0; i < formInput.length; i++){
         formInput[i].style.borderColor = "black";
         formLabel[i].style.color = "#393939";
         return;
-        
     });
 }
+
+checkPasswords();
+
+function checkPasswords(){
+    userConfirmedPassword.addEventListener('focusout', (e) =>{
+        if(userConfirmedPassword.value === userPassword.value){
+            dontMatchMessage.textContent = "";
+            return
+        }
+        userConfirmedPassword.style.borderColor = "#FF0000";
+        dontMatchMessage.textContent = "Password don't match";
+        
+    
+    });
+    userConfirmedPassword.addEventListener("keyup", (e) =>{
+        if(userConfirmedPassword.value === userPassword.value){
+            dontMatchMessage.textContent = "";
+            return;
+        }
+    });
+}
+
